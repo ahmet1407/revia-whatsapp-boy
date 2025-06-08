6720987827b9428869adbc9c31c72137c31168ec
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-08 15:53:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route('/message', methods=['POST'])
+@app.route("/message", methods=['POST'])
 def message():
     incoming_msg = request.values.get('Body', '').lower()
-    if 'dyson' in incoming_msg:
-        return jsonify({'response': 'Dyson V15 Detect: Tatmin 92, Risk 23, Hissiyat 88, Uzman Skoru 90'})
-    else:
-        return jsonify({'response': 'Üzgünüm, bu ürün için henüz bir değerlendirme yok.'})
+    resp = MessagingResponse()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    if 'dyson' in incoming_msg:
+        reply = "Dyson V15 Detect: Tatmin 92, Risk 23, Hissiyat 88, Uzman Skoru 90"
+    else:
+        reply = "Lütfen analiz etmem için geçerli bir ürün adı gönderin."
+
+    resp.message(reply)
+    return str(resp)
